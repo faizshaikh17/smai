2a77ebdf12a7d09b3ec6aa7773c270f847787805
Faiz Shaikh <mohdfaiz8101@gmail.com>
2025-06-23 00:02:42 +0530

    fix: button update

--- app/ui/components/Button.tsx
+++ app/ui/components/Button.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 interface CTAButtonProps {
     label: string;
-    label2?: string;
+    label2?: React.ReactNode;  // change here to allow SVG or JSX
     suplabel: string;
     href: string;
     target?: '_blank' | '_self';
@@ -27,11 +27,15 @@ const Button: React.FC<CTAButtonProps> = ({
             >
                 <div className="p-1 w-full flex items-center justify-center rounded-[0.85rem] bg-[#1C1F24] shadow-[inset_0_4px_6px_#242629,inset_0_1px_2px_rgba(255,255,255,0.04),inset_0_3px_2px_rgba(209,213,219,0.1),0_3px_10px_rgba(0,0,0,0.25),0_1px_1px_rgba(255,255,255,0.06)]">
                     <span className={`${className} font-medium text-base text-[var(--white)] px-4 py-2 bg-[#21252A] rounded-[0.7rem] flex items-center justify-center gap-4 shadow-[0_2px_6px_rgba(0,0,0,0.2),0_-1px_2px_rgba(209,213,219,0.05),inset_0_1px_0_rgba(255,255,255,0.01),0_-2px_3px_rgba(0,0,0,0.12)] transition-colors duration-200 hover:bg-[#2A2E36] w-full`}>
-                        <p>
+                        <p className="flex items-center gap-1">
                             {label}
                             <sup className={`${className2} ml-1 text-[0.65rem]`}>{suplabel}</sup>
                         </p>
-                        <p className='text-neutral-700 text-3xl'>{label2}</p>
+                        {label2 && (
+                            <span className="flex items-center justify-center text-neutral-700 text-3xl">
+                                {label2}
+                            </span>
+                        )}
                     </span>
                 </div>
             </button>
--- app/ui/components/Hero.tsx
+++ app/ui/components/Hero.tsx
@@ -32,6 +32,22 @@ const Hero: React.FC = () => {
                                 target="_blank"
                                 className="text-lg md:w-[18rem] w-full"
                                 suplabel="TM"
+                                label2={
+                                    <div className="flex items-center gap-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round"
+                                            className="lucide lucide-command h-4.5 w-4.5 text-white">
+                                            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3"></path>
+                                        </svg>
+
+                                        <svg width="2em" height="2em" fill="none" xmlns="http://www.w3.org/2000/svg"
+                                            className="mt-1.5 h-4.5 w-4.5 fill-white" viewBox="0 0 16 16">
+                                            <path d="M4.26465 11.0684C4.08008 11.0684 3.88184 10.9863 3.73828 10.8428L0.744141 7.90332C0.600586 7.75977 0.518555 7.55469 0.518555 7.35645C0.518555 7.1582 0.600586 6.95312 0.744141 6.81641L3.73828 3.87695C3.88184 3.7334 4.08008 3.64453 4.26465 3.64453C4.69531 3.64453 4.97559 3.94531 4.97559 4.35547C4.97559 4.57422 4.89355 4.73145 4.75684 4.86816L3.59473 5.98926L2.74707 6.68652L3.9707 6.625H10.335C10.8408 6.625 11.0391 6.42676 11.0391 5.9209V2.89941C11.0391 2.38672 10.8408 2.18848 10.335 2.18848H7.5459C7.11523 2.18848 6.80078 1.86035 6.80078 1.45703C6.80078 1.05371 7.11523 0.725586 7.5459 0.725586H10.3828C11.8594 0.725586 12.4814 1.34766 12.4814 2.82422V5.98926C12.4814 7.44531 11.8594 8.08789 10.3828 8.08789H3.9707L2.74707 8.0332L3.59473 8.72363L4.75684 9.85156C4.89355 9.98145 4.97559 10.1455 4.97559 10.3643C4.97559 10.7744 4.69531 11.0684 4.26465 11.0684Z"></path>
+                                        </svg>
+                                    </div>
+
+                                }
                             />
                         </div>
                         <div className="text-sm sm:text-base space-y-2 font-medium text-gray-300 md:text-left">
